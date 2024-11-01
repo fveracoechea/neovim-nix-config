67054033acb47cc2129e18258b97d7a1d47eba51
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2024-11-01 12:30:22 -0400

    Update winbar settings

--- lua/winbar.lua
+++ lua/winbar.lua
@@ -1,10 +1,9 @@
 local colors = require "catppuccin.palettes.mocha"
 
-vim.cmd(string.format([[highlight WinBar1 guifg=%s]], colors.rosewater))
-
-vim.cmd(string.format([[highlight WinBarM guifg=%s]], colors.green))
-
-vim.cmd(string.format([[highlight WinBar2 guifg=%s]], colors.peach))
+vim.cmd(string.format([[highlight WinBarPath guifg=%s]], colors.rosewater))
+vim.cmd(string.format([[highlight WinBarModified guifg=%s]], colors.green))
+vim.cmd(string.format([[highlight WinBarBuffers guifg=%s]], colors.peach))
+vim.cmd(string.format([[highlight WinBarIndicator guifg=%s]], colors.blue))
 
 local winbar_filetype_exclude = {
   [""] = true,
@@ -15,22 +14,6 @@ local winbar_filetype_exclude = {
   ["dashboard"] = true,
   ["neo-tree"] = true,
 }
-
---
--- local is_current = function()
---   local winid = vim.g.actual_curwin
---   if isempty(winid) then
---     return false
---   else
---     return winid == tostring(vim.api.nvim_get_current_win())
---   end
--- end
-
--- Function to get the full path and replace the home directory with ~
-local function get_winbar_path()
-  local full_path = vim.fn.expand "%:p"
-  return full_path:gsub(vim.fn.expand "$HOME", "~")
-end
 
 -- Function to get the number of open buffers using the :ls command
 local function get_buffer_count()
@@ -48,15 +31,43 @@ end
 -- Function to update the winbar
 local function update_winbar()
   if winbar_filetype_exclude[vim.bo.filetype] then
+    vim.opt.winbar = "%{%v:lua.winbar.active_indicator()%}"
     return
   end
 
-  local home_replaced = get_winbar_path()
   local buffer_count = get_buffer_count()
-  vim.opt.winbar = "%#WinBar2#(" .. buffer_count .. ") " .. "%#WinBarM#%m " .. "%#WinBar1#" .. "%f" .. "%*%=%#WinBar2#"
+  vim.opt.winbar = ""
+    .. "%#WinBarBuffers#("
+    .. buffer_count
+    .. ") "
+    .. "%#WinBarPath#"
+    .. "%f"
+    .. "%#WinBarModified#%m "
+    .. "%*%=%#WinBar2#"
 end
 
 -- Autocmd to update the winbar on BufEnter and WinEnter events
 vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter" }, {
   callback = update_winbar,
 })
+
+local M = {}
+
+local is_current = function()
+  local winid = vim.g.actual_curwin
+  if isempty(winid) then
+    return false
+  else
+    return winid == tostring(vim.api.nvim_get_current_win())
+  end
+end
+
+M.active_indicator = function()
+  if is_current() then
+    return "%#WinBarIndicator#▔▔▔▔▔▔▔▔%*"
+  else
+    return ""
+  end
+end
+
+return M
