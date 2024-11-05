79455595f9761606b34c82f19f5bf00fc06883dd
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2024-11-05 14:56:12 -0500

    Improve macro recording notifications

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -11,7 +11,16 @@ lualine.setup {
     lualine_a = { "mode" },
     lualine_b = { "branch", "diff", "diagnostics" },
     lualine_c = { "filename" },
-    lualine_x = { "lsp_progress", "fileformat", "filetype" },
+    lualine_x = {
+      "lsp_progress",
+      "fileformat",
+      "filetype",
+      {
+        require("noice").api.statusline.mode.get,
+        cond = require("noice").api.statusline.mode.has,
+        color = { fg = "#ff9e64" },
+      },
+    },
     lualine_y = { "progress" },
     lualine_z = { "location" },
   },
