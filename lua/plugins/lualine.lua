640ac1f790fd7895a9f69601c711c4f88ce3afb5
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2024-11-05 15:03:23 -0500

    update status line mode

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,23 +3,20 @@ local lualine = require "lualine"
 lualine.setup {
   options = {
     theme = "catppuccin",
-    -- component_separators = "",
     globalstatus = true,
     section_separators = { left = "", right = "" },
   },
   sections = {
-    lualine_a = { "mode" },
+    lualine_a = {
+      -- "mode",
+ ,     require("noice").api.statusline.mode.get,
+    },
     lualine_b = { "branch", "diff", "diagnostics" },
     lualine_c = { "filename" },
     lualine_x = {
       "lsp_progress",
       "fileformat",
       "filetype",
-      {
-        require("noice").api.statusline.mode.get,
-        cond = require("noice").api.statusline.mode.has,
-        color = { fg = "#ff9e64" },
-      },
     },
     lualine_y = { "progress" },
     lualine_z = { "location" },
