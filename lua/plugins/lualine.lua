fd8db8e98991fa1e5fbcbaaff3c6a21004ed5976
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2024-11-16 13:53:18 -0500

    tese

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -35,47 +35,7 @@ lualine.setup {
   winbar = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {
-      {
-        "buffers",
-        show_filename_only = true, -- Shows shortened relative path when set to false.
-        hide_filename_extension = false, -- Hide filename extension when set to true.
-        show_modified_status = true, -- Shows indicator when the buffer is modified.
-        mode = 0, -- 0: Shows buffer name
-        -- 1: Shows buffer index
-        -- 2: Shows buffer name + buffer index
-        -- 3: Shows buffer number
-        -- 4: Shows buffer name + buffer number
-
-        max_length = vim.o.columns * 2 / 3, -- Maximum width of buffers component,
-        -- it can also be a function that returns
-        -- the value of `max_length` dynamically.
-
-        -- Shows specific buffer name for that filetype ( { `filetype` = `buffer_name`, ... } )
-        filetype_names = {
-          TelescopePrompt = "Telescope",
-          dashboard = "Dashboard",
-          packer = "Packer",
-          fzf = "FZF",
-          alpha = "Alpha",
-        },
-
-        -- Automatically updates active buffer color to match color of other components (will be overidden if buffers_color is set)
-        use_mode_colors = false,
-
-        buffers_color = {
-          -- Same values as the general color option can be used here.
-          active = "lualine_{section}_normal", -- Color for active buffer.
-          inactive = "lualine_{section}_inactive", -- Color for inactive buffer.
-        },
-
-        symbols = {
-          modified = " ●", -- Text to show when the buffer is modified
-          alternate_file = "#", -- Text to show to identify the alternate file
-          directory = "", -- Text to show when the buffer is a directory
-        },
-      },
-    },
+    lualine_c = { "filename" },
     lualine_x = {},
     lualine_y = {},
     lualine_z = {},
