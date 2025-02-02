9351692186baade4853582e9d19a2a79aeeb4892
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2025-02-02 18:23:17 -0500

    add node-env

--- packages/default.nix
+++ packages/default.nix
@@ -1,6 +1,8 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
-pkgs:
-{
-  # example = pkgs.callPackage ./example { };
-}
-// (pkgs.callPackage ./node-packages.nix {})
+pkgs: let
+  nodeEnv = import ../node-env.nix pkgs;
+in
+  {
+    # example = pkgs.callPackage ./example { };
+  }
+  // (pkgs.callPackage ./node-packages.nix {inherit nodeEnv;})
