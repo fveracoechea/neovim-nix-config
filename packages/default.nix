1b97de6adaf00fc4643bc124fd82f634434d1501
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2025-02-02 18:26:40 -0500

    fix node-env call

--- packages/default.nix
+++ packages/default.nix
@@ -1,6 +1,22 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 pkgs: let
-  nodeEnv = import ../node-env.nix pkgs;
+  nodeEnv = import ./node-env.nix {
+    inherit
+      (pkgs)
+      stdenv
+      lib
+      python2
+      runCommand
+      writeTextFile
+      writeShellScript
+      nodejs
+      ;
+    inherit pkgs;
+    libtool =
+      if pkgs.stdenv.hostPlatform.isDarwin
+      then pkgs.cctools or pkgs.darwin.cctools
+      else null;
+  };
 in
   {
     # example = pkgs.callPackage ./example { };
