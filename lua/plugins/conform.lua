17fa92abc708b27dabd8709efa021337d36e5c84
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2025-04-30 16:19:21 -0400

    fix prettier formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,11 +1,13 @@
 local conform = require "conform"
 
+local prettier = { "prettierd", "prettier", stop_after_first = true }
+
 local js_formatters = function()
   if require("lspconfig").util.root_pattern("deno.json", "deno.jsonc")(vim.fn.getcwd()) then
     return { "deno_fmt" }
   end
 
-  return { "prettier" }
+  return prettier
 end
 
 conform.setup {
@@ -20,10 +22,10 @@ conform.setup {
     jsonc = js_formatters,
     yaml = js_formatters,
     markdown = js_formatters,
-    svelte = { "prettier" },
-    graphql = { "prettier" },
-    liquid = { "prettier" },
-    lua = { "stylua" },
+    svelte = prettier,
+    graphql = prettier,
+    liquid = prettier,
+    lua = prettier,
     python = { "isort", "black" },
     nix = { "alejandra" },
     ["*"] = { "codespell" },
