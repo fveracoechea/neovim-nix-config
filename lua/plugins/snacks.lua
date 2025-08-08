84794ab74d5c1dd0ba2cb3b791bb985d33f8bd0c
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2025-08-08 17:00:45 -0400

    update dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,6 +7,7 @@ snacks.setup {
     sections = {
       { section = "header" },
       { section = "keys", gap = 1, padding = 1 },
+      { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
       {
         section = "terminal",
         cmd = "zeitfetch --no-logo;",
@@ -14,7 +15,48 @@ snacks.setup {
         indent = 4,
         height = 30,
       },
-      { icon = " ", title = "Recent Files", pane = 2, section = "recent_files", indent = 2, padding = { 2, 2 } },
+      function()
+        local in_git = Snacks.git.get_root() ~= nil
+        local cmds = {
+          {
+            title = "Notifications",
+            cmd = "gh notify -s -a -n5",
+            action = function()
+              vim.ui.open "https://github.com/notifications"
+            end,
+            key = "n",
+            icon = " ",
+            height = 10,
+            enabled = true,
+          },
+          {
+            icon = " ",
+            title = "Open PRs",
+            cmd = "gh pr list -L 5",
+            key = "P",
+            action = function()
+              vim.fn.jobstart("gh pr list --web", { detach = true })
+            end,
+            height = 10,
+          },
+          {
+            icon = " ",
+            title = "Git Status",
+            cmd = "git --no-pager diff --stat -B -M -C",
+            height = 10,
+          },
+        }
+        return vim.tbl_map(function(cmd)
+          return vim.tbl_extend("force", {
+            pane = 2,
+            section = "terminal",
+            enabled = in_git,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+          }, cmd)
+        end, cmds)
+      end,
     },
   },
   preset = {
