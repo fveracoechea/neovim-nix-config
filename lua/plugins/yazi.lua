2992b854f19100484f8e46a1f53b6f7d1450c4da
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2025-08-08 10:08:29 -0400

    update yazi borders

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -2,4 +2,5 @@ require("yazi").setup {
   -- if you want to open yazi instead of netrw, see below for more info
   open_for_directories = false,
   floating_window_scaling_factor = 0.85,
+  yazi_floating_window_border = "shadow",
 }
