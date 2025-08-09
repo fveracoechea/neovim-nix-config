2b15b22ea41a3d4e1922b12e532a507a0feffe24
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2025-08-09 10:16:48 -0400

    update noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -11,8 +11,43 @@ require("noice").setup {
   -- Route notifications to snacks.notifier instead of noice's built-in notifier
   routes = {
     {
-      filter = { event = "notify" },
       view = "notify_send",
+      filter = { event = "notify" },
+    },
+    {
+      view = "notify",
+      filter = { event = "msg_showmode" },
+    },
+  },
+
+  views = {
+    cmdline_popup = {
+      position = {
+        row = 5,
+        col = "50%",
+      },
+      size = {
+        width = 60,
+        height = "auto",
+      },
+    },
+    popupmenu = {
+      relative = "editor",
+      position = {
+        row = 8,
+        col = "50%",
+      },
+      size = {
+        width = 60,
+        height = 10,
+      },
+      border = {
+        style = "rounded",
+        padding = { 0, 1 },
+      },
+      win_options = {
+        winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+      },
     },
   },
 }
