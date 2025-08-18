76277279be20b614936f36fa4423d178b4090a75
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2025-08-18 10:03:47 -0400

    remove redraw

--- lua/options.lua
+++ lua/options.lua
@@ -38,7 +38,6 @@ vim.opt.pumblend = 10 -- Popup menu transparency
 vim.opt.winblend = 0 -- Floating window transparency
 vim.opt.conceallevel = 0 -- Don't hide markup
 vim.opt.concealcursor = "" -- Don't hide cursor line markup
-vim.opt.lazyredraw = true -- Don't redraw during macros
 vim.opt.synmaxcol = 300 -- Syntax highlighting limit
 vim.opt.winborder = "rounded" -- Window border style
 
