1f292b6ad2cf67603a00255e518914ef7b622a49
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2025-10-10 14:02:31 -0400

    fix lsp imports

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -3,6 +3,16 @@ vim.g.markdown_fenced_languages = {
   "ts=typescript",
 }
 
+-- Sets global diagnostic config
+local x = vim.diagnostic.severity
+vim.diagnostic.config {
+  virtual_text = { prefix = "" },
+  -- Change the Diagnostic symbols in the sign column (gutter)
+  signs = { text = { [x.ERROR] = "󰅙 ", [x.WARN] = " ", [x.INFO] = " ", [x.HINT] = "󰠠 " } },
+  underline = true,
+  float = { border = "rounded" },
+}
+
 -- General LSP Keymaps with LspAttach Autocommand
 vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("UserLspConfig", {}),
@@ -54,6 +64,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
   end,
 })
 
+-- Enable the servers listed here that have been configured in `lua/lsp/*`
 vim.lsp.enable {
   "html",
   "nil_ls",
--- lua/utils/lsp-capabilities.lua
+++ lua/utils/lsp-capabilities.lua
@@ -1,6 +1,11 @@
-local default_capabilities = require("cmp_nvim_lsp").default_capabilities()
+require("lsp-file-operations").setup()
 
-local capabilities = vim.tbl_deep_extend("force", vim.lsp.protocol.make_client_capabilities(), default_capabilities)
+local capabilities = vim.tbl_deep_extend(
+  "force",
+  vim.lsp.protocol.make_client_capabilities(),
+  require("cmp_nvim_lsp").default_capabilities(),
+  require("lsp-file-operations").default_capabilities()
+)
 
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
