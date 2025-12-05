4a92be31d3f4d37f0d4a90013b1a390403bcfcc4
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2025-12-05 10:41:57 -0500

    update mappings

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -6,7 +6,19 @@ require("mini.sessions").setup {
 
 require("mini.pairs").setup {}
 
-require("mini.surround").setup {}
+require("mini.surround").setup {
+  mappings = {
+    add = "<leader>sa", -- Add surrounding in Normal and Visual modes
+    delete = "<leader>sd", -- Delete surrounding
+    find = "<leader>sf", -- Find surrounding (to the right)
+    find_left = "<leader>sF", -- Find surrounding (to the left)
+    highlight = "<leader>sh", -- Highlight surrounding
+    replace = "<leader>sr", -- Replace surrounding
+
+    suffix_last = "", -- Suffix to search with "prev" method
+    suffix_next = "", -- Suffix to search with "next" method
+  },
+}
 
 require("mini.comment").setup {}
 require("mini.diff").setup {}
