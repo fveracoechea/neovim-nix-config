87fb34c1706c3a96e83ef41b312e968b925b0860
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2025-12-05 10:24:38 -0500

    update surround config

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -5,17 +5,9 @@ require("mini.sessions").setup {
 }
 
 require("mini.pairs").setup {}
-require("mini.surround").setup {
-  mappings = {
-    add = "sa", -- Add surrounding in Normal and Visual modes
-    delete = "sd", -- Delete surrounding
-    find = "sf", -- Find surrounding (to the right)
-    find_left = "sF", -- Find surrounding (to the left)
-    highlight = "sh", -- Highlight surrounding
-    replace = "sr", -- Replace surrounding
-    update_n_lines = "sn", -- Update `n_lines`
-  },
-}
+
+require("mini.surround").setup {}
+
 require("mini.comment").setup {}
 require("mini.diff").setup {}
 
