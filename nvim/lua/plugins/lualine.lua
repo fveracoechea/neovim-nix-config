10d60f733b9b313607818c41ab3a6fda78a42d08
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2026-03-29 18:48:39 -0400

    update lualine setup

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,7 +1,5 @@
 local lualine = require "lualine"
 
-local colors = require "catppuccin.palettes.mocha"
-
 -- Function to get the number of open buffers using the :ls command
 local function get_buffer_count()
   local buffers = vim.fn.execute "ls"
@@ -22,22 +20,11 @@ lualine.setup {
     theme = "catppuccin-nvim",
     globalstatus = true,
     section_separators = { left = "", right = "" },
-    disabled_filetypes = { winbar = { "NvimTree", "alpha", "snacks_explorer" } },
+    disabled_filetypes = { winbar = { "NvimTree", "alpha", "snacks_explorer", "snacks_picker_list" } },
   },
 
   sections = {
     lualine_a = { "mode" },
-    lualine_b = { "branch", "diff" },
-    lualine_c = { "lsp_status" },
-    lualine_x = { "filesize", "encoding" },
-    lualine_y = { "filetype" },
-    lualine_z = { "location" },
-  },
-
-  winbar = {
-    lualine_a = {
-      get_buffer_count,
-    },
     lualine_b = {
       {
         "filename",
@@ -45,27 +32,11 @@ lualine.setup {
         newfile_status = true,
         path = 1,
       },
-      "diagnostics",
     },
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = {},
-  },
+    lualine_c = { "diagnostics" },
 
-  inactive_winbar = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {
-      {
-        "filename",
-        file_status = true,
-        newfile_status = true,
-        path = 1,
-        color = { fg = colors.subtext1 },
-      },
-    },
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = {},
+    lualine_x = { "lsp_status", "filesize", "encoding" },
+    lualine_y = { "location" },
+    lualine_z = { get_buffer_count },
   },
 }
